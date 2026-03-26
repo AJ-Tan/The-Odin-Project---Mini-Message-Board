fb2f474ca6a28adcf4319f2f3c5aeb7e4c8f6f9d
AJ-Tan <aldomin22@gmail.com>
2026-03-26 14:00:33 +0800

    Adjusted connection string to be based on .env

--- Components/models/db/pool.js
+++ Components/models/db/pool.js
@@ -1,4 +1,4 @@
 const { Pool } = require("pg");
 module.exports = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:${process.env.DB_PORT}/odin-project-dbs`,
+  connectionString: process.env.DB_URL,
 });
